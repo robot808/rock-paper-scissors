cffa9b596161b4b0c2126aee82216c67583fb9d2
robot808 <bob@cyber-wizard.com>
2023-04-19 02:21:53 -0500

    Add alert to getComputerChoice function

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function getComputerChoice() {
   // get a random whole number between 1 and 3
-  const choice = Math.ceil(Math.random() * 3);
+  const random = Math.ceil(Math.random() * 3);
 
   // 1 for rock, 2 for paper, 3 for scissors
-  switch (choice) {
+  let choice;
+
+  switch (random) {
     case 1:
-      return "rock";
+      choice = "rock";
     case 2:
-      return "paper";
+      choice = "paper";
     case 3:
-      return "scissors";
-    default:
-      return;
-  } 
+      choice = "scissors";
+  }
+
+  // alert the player what was chosen
+  alert(`The computer has chosen ${choice}.`);
+
+  // return the computer's choice
+  return choice; 
 }
