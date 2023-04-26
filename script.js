3bf57581e56db4aa3adceb321368e30104ca04d0
robot808 <rsbradshaw88@protonmail.com>
2023-04-26 18:39:21 -0500

    Add displayScore function from refactored code

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+function displayScore () {
+  score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+}
+
+function checkScore () {
+  if (playerScore >= 5) {
+    game.textContent = "You won the game! Congratulations!";
+    resetGame();
+  }
+  else if (computerScore >= 5) {
+    game.textContent = "You lost the game! Better luck next time!";
+    resetGame();
+  }
+  else {
+    game.textContent = "";
+  }
+}
+
+function makeChoice (event) {
+  const playerChoice = event.target.id.toString();
+  const score = playRound(playerChoice, getComputerChoice());
+  if (score) {
+    switch (score) {
+      case 1:
+        playerScore++;
+        break;
+      default:
+        computerScore++;
+    }
+  }
+  displayScore();
+  checkScore();
+}
+
 function getComputerChoice() {
   // get a random whole number between 1 and 3
   const random = Math.ceil(Math.random() * 3);
@@ -76,104 +115,15 @@ function playRound (playerChoice, computerChoice) {
   return score;
 }
 
-function makeChoice (event) {
-  const playerChoice = event.target.id.toString();
-  const score = playRound(playerChoice, getComputerChoice());
-  if (score) {
-    switch (score) {
-      case 1:
-        playerScore++;
-        break;
-      default:
-        computerScore++;
-    }
-  }
-  checkScore();
-}
-
-function checkScore () {
-  score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
-
-  if (playerScore >= 5) {
-    game.textContent = "You won the game! Congratulations!";
-    resetGame();
-  }
-  else if (computerScore >= 5) {
-    game.textContent = "You lost the game! Better luck next time!";
-    resetGame();
-  }
-  else {
-    game.textContent = "";
-  }
-}
-
-function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-}
-
-/*
-function game() {
-  let computerScore = 0;
-  let playerScore = 0;
-
-  // play five rounds, keeping track of the score
-  for (let i = 0; i < 5; i++) {
-    let result = playRound(getPlayerChoice(), getComputerChoice()).slice(4, 5);
-
-    switch (result) {
-      case "l":
-        ++computerScore;
-        console.log(`Round ${i + 1}: computer`);
-        break;
-      case "w":
-        ++playerScore;
-        console.log(`Round ${i + 1}: player`);
-        break;
-      default:
-        console.log(`Round ${i + 1}: draw`);
-    }
-  }
-
-  // alert the player of the game result
-  if (computerScore > playerScore) {
-    alert("You lost the game! Better luck next time!");
-  }
-  else if (playerScore > computerScore) {
-    alert("You won the game! Congratulations!");
-  }
-  else {
-    alert("The game ended in a tie! Play again!");
-  }
-}
-*/
-/*
-const rock = document.querySelector("#rock");
-rock.addEventListener("click", () => {
-  playRound("rock", getComputerChoice());
-});
-
-const paper = document.querySelector("#paper");
-paper.addEventListener("click", () => {
-  playRound("paper", getComputerChoice());
-});
-
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", () => {
-  playRound("scissors", getComputerChoice());
-});
-*/
-
 const score = document.querySelector("div.score");
-const round = document.querySelector("div.round");
-const game = document.querySelector("div.game");
-const buttons = document.querySelectorAll(".buttons button");
-
 let playerScore = 0;
 let computerScore = 0;
+displayScore();
 
-checkScore();
+const round = document.querySelector("div.round");
+const game = document.querySelector("div.game");
 
+const buttons = document.querySelectorAll(".buttons button");
 buttons.forEach((button) => {
   button.addEventListener("click", makeChoice);
 });
