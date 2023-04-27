e3f19c97e172c8bf275f574588d38d1e2a498115
robot808 <rsbradshaw88@protonmail.com>
2023-04-26 19:16:04 -0500

    Add reset button to trigger resetGame function

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function displayScore () {
+  // update player and computer scores in score div
   score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 }
 
 function resetGame() {
+  // return game to original state
   playerScore = 0;
   computerScore = 0;
+  displayScore();
+  round.innerText = "";
+  game.innerText = "";
+  reset.style.display = "none"; // re-hide reset button
 }
 
 function checkScore () {
-  if (playerScore >= 5) {
-    game.textContent = "You won the game! Congratulations!";
-    resetGame();
+  // diplay winner in game div
+  const winningScore = 5;
+  if (playerScore >= winningScore) {
+    game.innerText += `You won the game! Congratulations!
+                        Click the reset button below to play again!`;
+    reset.style.display = "block"; // unhide reset button
   }
-  else if (computerScore >= 5) {
-    game.textContent = "You lost the game! Better luck next time!";
-    resetGame();
+  else if (computerScore >= winningScore) {
+    game.innerText += `You lost the game! Better luck next time!
+                        Click the reset button below to play again!`;
+    reset.style.display = "block"; // unhide reset button
   }
   else {
     game.textContent = "";
@@ -22,6 +32,9 @@ function checkScore () {
 }
 
 function makeChoice (event) {
+  if (game.innerText) return; // do nothing if awaiting game reset
+
+  // get player choice from button id and play a round
   const playerChoice = event.target.id.toString();
   const score = playRound(playerChoice, getComputerChoice());
   if (score) {
@@ -34,16 +47,15 @@ function makeChoice (event) {
     }
   }
   displayScore();
-  checkScore();
+  checkScore(); // determine if someone has won
 }
 
 function getComputerChoice() {
   // get a random whole number between 1 and 3
   const random = Math.ceil(Math.random() * 3);
-
-  // 1 for rock, 2 for paper, 3 for scissors
+  
   let choice;
-
+  // 1 for rock, 2 for paper, 3 for scissors
   switch (random) {
     case 1:
       choice = "rock";
@@ -55,12 +67,10 @@ function getComputerChoice() {
       choice = "scissors";
   }
 
-  // return the computer's choice
   return choice; 
 }
 
 function playRound (playerChoice, computerChoice) {
-  // determine the result
   let result = `You have chosen ${playerChoice}!
                 The computer has chosen ${computerChoice}!\n`;
   let win = `You win! ${playerChoice} beats ${computerChoice}!`;
@@ -68,6 +78,7 @@ function playRound (playerChoice, computerChoice) {
   let draw = "It's a draw!";
   let score = 0;
 
+  // determine the result
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
       result += draw;
@@ -108,22 +119,24 @@ function playRound (playerChoice, computerChoice) {
     }
   }
 
-  // alert the player of the result
+  // display the result of the round in round div
   round.innerText = result;
 
-  // return the result
   return score;
 }
 
-const score = document.querySelector("div.score");
+const score = document.querySelector(".score p");
 let playerScore = 0;
 let computerScore = 0;
 displayScore();
 
-const round = document.querySelector("div.round");
-const game = document.querySelector("div.game");
+const round = document.querySelector(".round p");
+const game = document.querySelector(".game p");
+const reset = document.querySelector("button#reset");
 
 const buttons = document.querySelectorAll(".buttons button");
 buttons.forEach((button) => {
   button.addEventListener("click", makeChoice);
 });
+
+reset.addEventListener("click", resetGame);
